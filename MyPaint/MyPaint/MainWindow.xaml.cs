5406e009dcee7a52be13ea02b2dea8f759a7661d
thole20042001 <nhattho03@gmail.com>
2021-12-26 21:59:10 +0700

    first commit

---
+++ MyPaint/MyPaint/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPaint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Fluent.RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RibbonWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 
+        }
+
+        private void _mainRibbon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void OnLauncherButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMenuItemClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pasteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void selectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
